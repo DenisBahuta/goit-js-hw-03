b0732b6950f4d85a09ca7b353a7cd43d27dd56e2
DenisBahuta <denisboguto@gmail.com>
2023-11-09 21:02:11 +0100

    JS97-Homework 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,14 @@
-function checkForSpam(message){
-    const hasSpam = message.toLowerCase().includes('spam');
-    const hasSale = message.toLowerCase().includes('sale');
-  
-    if (hasSpam || hasSale) {
-      return true;
-    } else {
-      return false;
+function filterArray(numbers, value){
+  const array = [];
+  for (let i of numbers){
+    if (i > value){
+        array.push(i);
     }
-  }
-  
-  
-  console.log(checkForSpam("Latest technology news")); // false
-  console.log(checkForSpam("JavaScript weekly newsletter")); // false
-  console.log(checkForSpam("Get best sale offers now!")); // true
-  console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-  console.log(checkForSpam("Trust me, this is not a spam message")); // true
-  console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-  console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+} return array;
+  } 
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
